c3e226f6548bffec392ada7c09e127d4803f6648
adentintime <arixbizz@gmail.com>
2018-07-22 20:39:34 +0000

    Add back files

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    Define the class City.
+'''
+from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+
+
+class City(BaseModel, Base):
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
+    state = relationship("State", back_populates="cities")
---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    Implementation of the State class
+'''
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel, Base):
+    '''
+        Implementation for the State.
+    '''
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", primaryjoin="state_id=State.id", cascade="delete")
