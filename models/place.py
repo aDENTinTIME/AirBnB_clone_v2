a8592b1db2773abd5645988a66706e0b088f0c1f
bchen528 <bchen720@gmail.com>
2018-07-23 06:17:35 +0000

    add relationships

--- models/place.py
+++ models/place.py
@@ -2,21 +2,33 @@
 '''
     Define the class Place.
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     '''
         Define the class Place that inherits from BaseModel.
     '''
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+#    print(storage)
+#    print(type(storage))
+#    print(type(storage).__name__)
+if type(storage) == 'DBStorage':
+    review = relationship("Review", backref="place", cascade="delete")
+else:
+    a_list = []
+    for review, place in session.query(Review, Place).\
+        filter(Review.place_id == Place.id).all():
+        a_list.append(review)
