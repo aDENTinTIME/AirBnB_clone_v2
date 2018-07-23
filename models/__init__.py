635b433155af5d32bf5e2e1ff8ea06901bb460a6
bchen528 <bchen720@gmail.com>
2018-07-23 01:45:09 +0000

    revert changes for testing

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@
     Package initializer
 '''
 
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -10,21 +11,11 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-from os import getenv
-
 
 classes = {"User": User, "BaseModel": BaseModel,
            "Place": Place, "State": State,
            "City": City, "Amenity": Amenity,
            "Review": Review}
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
-
-if storage_type == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-    storage.reload()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-    storage.reload()
+storage = FileStorage()
+storage.reload()
--- models/city.py
+++ models/city.py
@@ -3,14 +3,11 @@
     Define the class City.
 '''
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
-    state = relationship("State", back_populates="cities")
+    state_id = ""
+    name = ""
