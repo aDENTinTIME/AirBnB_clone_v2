6961de9bdf4bebbeb79fad825c44ab91a48f47b0
bchen528 <bchen720@gmail.com>
2018-07-24 17:21:16 +0000

    update init fx

--- models/base_model.py
+++ models/base_model.py
@@ -5,26 +5,41 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     '''
         Base class for other classes to be used for the duration.
     '''
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         '''
             Initialize public instance attributes.
         '''
-        if (len(kwargs) == 0):
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.created_at = self.updated_at = datetime.now()
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            if kwargs.get('created_at'):
+                kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                         "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get('updated_at'):
+                kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                         "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.utcnow()
+
+            if not kwargs.get('id'):
+                self.id = str(uuid.uuid4())
+
             for key, val in kwargs.items():
                 if "__class__" not in key:
                     setattr(self, key, val)
@@ -48,6 +63,7 @@ class BaseModel:
             Update the updated_at attribute with new.
         '''
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -58,5 +74,10 @@ class BaseModel:
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        for k, v in cp_dct.items():
+            if k == '_sa_instance_state':
+                del v
+        return cp_dct
 
-        return (cp_dct)
+    def delete(self):
+        models.storage.delete(self)
