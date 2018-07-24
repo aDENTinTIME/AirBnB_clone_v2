41066698e75d2f8726a914eef7e4eb87090b3098
bchen528 <bchen720@gmail.com>
2018-07-24 19:05:38 +0000

    add sqlalchemy

--- models/place.py
+++ models/place.py
@@ -2,21 +2,49 @@
 '''
     Define the class Place.
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship, sessionmaker
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     '''
         Define the class Place that inherits from BaseModel.
     '''
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = 'places'
+    if storage_type == 'db':
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
+        #review = relationship("Review", backref="place",
+        #                      cascade="all, delete-orphan")
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+        """
+        a_list = []
+        session = Session()
+        for review, place in session.query(Review, Place).\
+            filter(Review.place_id == Place.id).all():
+            a_list.append(review)
+        """
