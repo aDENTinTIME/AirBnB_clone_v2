018ff75058cc82b76f1df98fc51a713b0119844c
bchen528 <bchen720@gmail.com>
2018-07-24 17:22:20 +0000

    specify db or filestorage

--- models/city.py
+++ models/city.py
@@ -2,12 +2,23 @@
 '''
     Define the class City.
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+#from models.state import State
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
-class City(BaseModel):
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class City(BaseModel, Base):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        name = ""
+        state_id = ""
