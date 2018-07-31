850e837479eb16672bfdde6c8860e4b75e324783
adentintime <arixbizz@gmail.com>
2018-07-31 06:44:41 +0000

    Update 0 with file removal fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static
-sudo apt-get -y update 1&> /dev/null
-sudo apt-get -y install Nginx 1&> /dev/null
+sudo apt-get -y update > /dev/null
+sudo apt-get -y install Nginx > /dev/null
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart 1&> /dev/null
+sudo service nginx restart > /dev/null
