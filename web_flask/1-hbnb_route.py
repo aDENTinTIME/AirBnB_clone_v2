bb1b7de1d7fe4c6931de911f3f6bb208035ec398
adentintime <arixbizz@gmail.com>
2018-08-15 22:50:07 +0000

    Update 1-3, won't execute upon import

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,15 +7,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    strict_slashes = False
-    return "Hello HBNB!"
+if __name__ == "__main__":
+    @app.route("/")
+    def hello():
+        strict_slashes = False
+        return "Hello HBNB!"
 
+    @app.route("/hbnb")
+    def hbnb():
+        strict_slashes = False
+        return "HBNB"
 
-@app.route("/hbnb")
-def hbnb():
-    strict_slashes = False
-    return "HBNB"
-
-app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
