b44d1c1b02938abec56f081511ae36525f30f5d9
adentintime <arixbizz@gmail.com>
2018-08-15 19:32:49 +0000

    Update 0-3, pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
+
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    strict_slashes=False
+    strict_slashes = False
     return "Hello HBNB!"
 
 app.run(host="0.0.0.0", port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
+
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    strict_slashes=False
+    strict_slashes = False
     return "Hello HBNB!"
+
 
 @app.route("/hbnb")
 def hbnb():
-    strict_slashes=False
+    strict_slashes = False
     return "HBNB"
 
 app.run(host="0.0.0.0", port=5000)
