19b586fe0a4f968db8bf255ff01a699f0929b783
adentintime <arixbizz@gmail.com>
2018-08-15 19:25:18 +0000

    Create 0-3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    strict_slashes=False
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    strict_slashes=False
+    return "HBNB"
+
+@app.route("/c/<text>")
+def c_text(text):
+    strict_slashes=False
+    return text.replace("_", " ")
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_text(text="is cool"):
+    strict_slashes=False
+    return "Python " + text.replace("_", " ")
+
+app.run(host="0.0.0.0", port=5000)
