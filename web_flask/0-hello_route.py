adc82f6082b2ed582c33597328204ff223037955
adentintime <arixbizz@gmail.com>
2018-08-15 22:13:19 +0000

    Update 0, trying to pass checker, now won't execute on import

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 
-from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    strict_slashes = False
-    return "Hello HBNB!"
+if __name__ == "__main__":
+    @app.route("/")
+    def hello():
+        strict_slashes = False
+        return "Hello HBNB!"
 
-app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
