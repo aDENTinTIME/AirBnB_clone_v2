7e373ad8604086501cdd4f468ec1ed15cd00c357
adentintime <arixbizz@gmail.com>
2018-08-16 03:39:33 +0000

    Create 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+if __name__ == "__main__":
+    @app.route("/")
+    def hello():
+        return "Hello HBNB!"
+
+    @app.route("/hbnb")
+    def hbnb():
+        return "HBNB"
+
+    @app.route("/c/<text>")
+    def c_text(text):
+        return text.replace("_", " ")
+
+    @app.route("/python/")
+    @app.route("/python/<text>")
+    def python_text(text="is cool"):
+        return "Python " + text.replace("_", " ")
+
+    @app.route("/number/<int:n>")
+    def number_text(n):
+        return "{} is a number".format(n)
+
+    @app.route("/number_template/<int:n>")
+    def number_template(n):
+        return render_template("5-number.html", number=n)
+
+    @app.route("/number_odd_or_even/<int:n>")
+    def number_odd_or_even(n):
+        return render_template("6-number_odd_or_even.html", num=n)
+
+    app.run(host="0.0.0.0", port=5000)
