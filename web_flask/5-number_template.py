2207d9158ac98326896a1e9da4da14247d197c12
adentintime <arixbizz@gmail.com>
2018-08-16 03:00:57 +0000

    Update 5 and templates, use render

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
 
 if __name__ == "__main__":
     @app.route("/")
     def hello():
-        strict_slashes = False
         return "Hello HBNB!"
 
     @app.route("/hbnb")
     def hbnb():
-        strict_slashes = False
         return "HBNB"
 
     @app.route("/c/<text>")
     def c_text(text):
-        strict_slashes = False
         return text.replace("_", " ")
 
     @app.route("/python/")
     @app.route("/python/<text>")
     def python_text(text="is cool"):
-        strict_slashes = False
         return "Python " + text.replace("_", " ")
 
     @app.route("/number/<int:n>")
     def number_text(n):
-        strict_slashes = False
         return "{} is a number".format(n)
 
     @app.route("/number_template/<int:n>")
     def number_template(n):
-        strict_slashes = False
-        with open("templates/5-number.html") as f:
-            d = f.read()
-            return d.format(number=n)
+        return render_template("5-number.html", number=n)
 
     app.run(host="0.0.0.0", port=5000)
